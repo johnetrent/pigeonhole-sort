c1e523f335a9e5bb4b93f51ed4a0180f16112909
John Trent <johnt@hzdg.com>
2016-10-14 15:13:34 -0400

    Add additional test cases

--- test.js
+++ test.js
@@ -3,9 +3,21 @@ import test from 'ava';
 import pigeonholeSort from './index';
 
 test('sorts arrays', t => {
-  const unsortedArray = [721, 4, 117, 13, 10, 880, 4, 117, 88];
-  const sortedArray = [4, 4, 10, 13, 88, 117, 117, 721, 880];
-  t.deepEqual(pigeonholeSort(unsortedArray), sortedArray);
+  {
+    const unsortedArray = [5, 3, 1, 2, 4];
+    const sortedArray = [1, 2, 3, 4, 5];
+    t.deepEqual(pigeonholeSort(unsortedArray), sortedArray);
+  }
+  {
+    const unsortedArray = [777, 999, 777, 55, 44, 11, 22, 888];
+    const sortedArray = [11, 22, 44, 55, 777, 777, 888, 999];
+    t.deepEqual(pigeonholeSort(unsortedArray), sortedArray);
+  }
+  {
+    const unsortedArray = [721, 4, 117, 13, 10, 880, 4, 117, 88];
+    const sortedArray = [4, 4, 10, 13, 88, 117, 117, 721, 880];
+    t.deepEqual(pigeonholeSort(unsortedArray), sortedArray);
+  }
 });
 
 test('throws an error when input is not an array', t => {
